b7abf51975283bae3a2399625f1841d4db5b1c7a
BinaryHunter <liyuwencool@163.com>
2020-04-10 16:31:07 +0800

    feat: autocommit

--- db-config-starter/src/main/java/pers/binaryhunter/db/mybatis/datasource/DataSourceProxy.java
+++ db-config-starter/src/main/java/pers/binaryhunter/db/mybatis/datasource/DataSourceProxy.java
@@ -255,7 +255,7 @@ public class DataSourceProxy implements DataSource {
 				if (method.getName().equals("commit")) {
 					Map<String, Connection> connectionMap = ConnectionHolder.CONNECTION_CONTEXT.get();
 					Connection writeCon = connectionMap.get(ConnectionHolder.WRITE);
-					if (writeCon != null && !this.autoCommit) {
+					if (writeCon != null && null != this.autoCommit && !this.autoCommit) {
 						writeCon.commit();
 					}
 					return null;
