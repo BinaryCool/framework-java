962928020e620713e9dbd300d92d7442bc26157e
BinaryHunter <liyuwencool@163.com>
2020-08-07 10:21:13 +0800

    feat: generic controller crud

--- framework-starter/src/main/java/pers/binaryhunter/framework/controller/GenericController.java
+++ framework-starter/src/main/java/pers/binaryhunter/framework/controller/GenericController.java
@@ -2,18 +2,27 @@ package pers.binaryhunter.framework.controller;
 
 import com.alibaba.fastjson.JSON;
 import org.apache.catalina.connector.ClientAbortException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import pers.binaryhunter.framework.bean.dto.paging.Page;
+import pers.binaryhunter.framework.bean.po.PO;
 import pers.binaryhunter.framework.bean.vo.ResponseBean;
+import pers.binaryhunter.framework.bean.vo.paging.PageResult;
 import pers.binaryhunter.framework.exception.BusinessException;
 import pers.binaryhunter.framework.exception.SessionOutException;
+import pers.binaryhunter.framework.service.GenericService;
+import pers.binaryhunter.framework.utils.MapConverter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 控制器父类
@@ -105,5 +114,80 @@ public class GenericController {
         } catch (Exception ex) {
             IOUtils.closeQuietly(pw);
         }
+    }
+
+    /**
+     * 分页查询
+     * @param service service
+     * @param bean 参数
+     * @param page 分页
+     * @return 分页结果
+     */
+    protected PageResult retrieve(GenericService service, Object bean, Page page) {
+        Map<String, Object> params = MapConverter.convertByField(bean);
+        return service.pageByArgs(params, page);
+    }
+
+    /**
+     * 新增
+     * @param service service
+     * @param bean 参数
+     * @return 返回对象
+     */
+    protected PO create(GenericService service, PO bean) {
+        if(null == bean) {
+            throw new BusinessException();
+        }
+        service.add(bean);
+
+        bean.setCreateTime(new Date());
+        bean.setStatus(PO.STATUS_ENABLE);
+        return bean;
+    }
+
+    /**
+     * 修改
+     * @param service service
+     * @param bean 参数
+     * @return 返回对象
+     */
+    protected PO update(GenericService service, PO bean) {
+        if(null ==  bean.getId() || bean.getId() <= 0) {
+            throw new BusinessException();
+        }
+        service.updateNotNull(bean);
+        bean = (PO) service.getById(bean.getId());
+        return bean;
+    }
+
+    /**
+     * 删除
+     * @param service service
+     * @param ids 删除的ID
+     * @return 返回删除的ID
+     */
+    protected Long[] delete(GenericService service, Long[] ids) {
+        if (null == ids || ids.length <= 0) {
+            throw new BusinessException();
+        }
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("idIn", StringUtils.join(ids, ","));
+        service.updateByArgs("t.status = " + PO.STATUS_DISABLE, params);
+        return ids;
+    }
+
+    /**
+     * 通过ID获取
+     * @param service service
+     * @param id ID
+     * @return 返回对象
+     */
+    protected PO get(GenericService service, Long id) {
+        if(null == id || id <= 0) {
+            throw new BusinessException();
+        }
+
+        return (PO) service.getById(id);
     }
 }
