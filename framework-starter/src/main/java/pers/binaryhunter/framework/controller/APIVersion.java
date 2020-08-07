c8e2469733b404943d5d3c478412d9faa7de3ecf
BinaryHunter <liyuwencool@163.com>
2020-08-07 11:08:11 +0800

    feat: APIVersionHandlerMapping

---
+++ framework-starter/src/main/java/pers/binaryhunter/framework/controller/APIVersion.java
@@ -0,0 +1,13 @@
+package pers.binaryhunter.framework.controller;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface APIVersion {
+    String[] value();
+}
---
+++ framework-starter/src/main/java/pers/binaryhunter/framework/controller/APIVersionHandlerMapping.java
@@ -0,0 +1,45 @@
+package pers.binaryhunter.framework.controller;
+
+
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.Method;
+
+public class APIVersionHandlerMapping extends RequestMappingHandlerMapping {
+    //private static final Logger log = LoggerFactory.getLogger(GenericController.class);
+
+    @Override
+    protected boolean isHandler(Class<?> beanType) {
+        return AnnotatedElementUtils.hasAnnotation(beanType, Controller.class);
+    }
+
+    @Override
+    protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
+        Class<?> controllerClass = method.getDeclaringClass();
+        //类上的APIVersion注解
+        APIVersion apiVersion = AnnotationUtils.findAnnotation(controllerClass, APIVersion.class);
+        //方法上的APIVersion注解
+        APIVersion methodAnnotation = AnnotationUtils.findAnnotation(method, APIVersion.class);
+        //以方法上的注解优先
+        if (methodAnnotation != null) {
+            apiVersion = methodAnnotation;
+        }
+
+        String[] urlPatterns = apiVersion == null ? new String[0] : apiVersion.value();
+
+        PatternsRequestCondition apiPattern = new PatternsRequestCondition(urlPatterns);
+        PatternsRequestCondition oldPattern = mapping.getPatternsCondition();
+        PatternsRequestCondition updatedFinalPattern = oldPattern.combine(apiPattern);
+
+        //重新构建RequestMappingInfo
+        mapping = new RequestMappingInfo(mapping.getName(), updatedFinalPattern, mapping.getMethodsCondition(),
+                mapping.getParamsCondition(), mapping.getHeadersCondition(), mapping.getConsumesCondition(),
+                mapping.getProducesCondition(), mapping.getCustomCondition());
+        super.registerHandlerMethod(handler, method, mapping);
+    }
+}
