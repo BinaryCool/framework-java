40d854199fe7389f4cf277873920e5e4e0fe0619
skbird <137621830@qq.com>
2021-03-08 17:29:01 +0800

    mybatis 分表插件

---
+++ db-config-starter/src/main/java/pers/binaryhunter/db/mybatis/shardbatis/strategy/impl/NoShardStrategy.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package pers.binaryhunter.db.mybatis.shardbatis.strategy.impl;
+
+import pers.binaryhunter.db.mybatis.shardbatis.strategy.ShardStrategy;
+
+/**
+ * 不进行分表的策略，供测试用
+ * @author sean.he
+ *
+ */
+public class NoShardStrategy implements ShardStrategy {
+
+	/* (non-Javadoc)
+	 * @see com.google.code.shardbatis.strategy.ShardStrategy#getTargetTableName(java.lang.String, java.lang.Object, java.lang.String)
+	 */
+	public String getTargetTableName(String baseTableName, Object params,
+			String mapperId) {
+		return baseTableName;
+	}
+
+}
