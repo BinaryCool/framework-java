ff75140c7c415f4ee9866f0f00e8484b4eb2e999
BinaryHunter <liyuwencool@163.com>
2021-03-29 11:41:28 +0800

    feat(enumutil): get by field

--- framework-starter/src/main/java/pers/binaryhunter/framework/utils/EnumUtil.java
+++ framework-starter/src/main/java/pers/binaryhunter/framework/utils/EnumUtil.java
@@ -1,5 +1,8 @@
 package pers.binaryhunter.framework.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
@@ -9,16 +12,56 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 工具类
- * @author 123456
- *
+ * EnumUtil
  */
 public class EnumUtil {
+    private static final Logger log = LoggerFactory.getLogger(EnumUtil.class);
+
+    public static <T> T getByValue(Class<T> enumT, Object fieldValue) {
+        return getByField(enumT, "value", fieldValue);
+    }
+
+    /**
+     * 通过字段名称和字段值获取枚举
+     */
+    public static <T> T getByField(Class<T> enumT, String fieldName, Object fieldValue) {
+        if (!enumT.isEnum()) {
+            return null;
+        }
+
+        AssertUtil.notBlank(fieldName);
+        AssertUtil.notNull(fieldValue);
+
+        T[] enums = enumT.getEnumConstants();
+        if (enums == null || enums.length <= 0) {
+            return null;
+        }
+
+        Field[] filedArr = enumT.getDeclaredFields();
+        for (int i = 0, len = enums.length; i < len; i++) {
+            try {
+                T tObj = enums[i];
+                for (Field f : filedArr) {
+                    if (Modifier.isStatic(f.getModifiers())) { // 跳过静态属性
+                        continue;
+                    }
+
+                    String name = f.getName();
+                    /**获取value值*/
+                    Object value = getMethodValue(name, tObj);
+                    if (fieldName.equals(name) && fieldValue.equals(value)) {
+                        return tObj;
+                    }
+                }
+            } catch (Exception e) {
+                log.error("", e);
+            }
+        }
+        return null;
+    }
+
     /**
      * 枚举转map结合value作为map的key,description作为map的value
-     *
-     * @param enumT
-     * @return enum mapcolloction
      */
     public static <T> List<Map<String, Object>> enumToList(Class<T> enumT) {
         List<Map<String, Object>> resultList = new ArrayList<>();
@@ -47,17 +90,14 @@ public class EnumUtil {
                 }
                 resultList.add(map);
             } catch (Exception e) {
-                e.printStackTrace();
+                log.error("", e);
             }
         }
         return resultList;
     }
 
     /**
-     * @param methodName
-     * @param obj
-     * @param args
-     * @return return value
+     * 通过方法和参数, 获取值
      */
     private static <T> Object getMethodValue(String methodName, T obj, Object... args) {
         try {
@@ -68,7 +108,7 @@ public class EnumUtil {
             }
             return method.invoke(obj, args);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("", e);
         }
         return null;
     }
